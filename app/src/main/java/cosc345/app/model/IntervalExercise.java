1dbdf34ac6dbce3f2f90b4733669cd658d19b571
Jmannn <manjo773@student.otago.ac.nz>
2018-09-12 02:02:40 +1200

    Simplified but could not change this.super, super(no args) must be 1st

--- app/src/main/java/cosc345/app/model/IntervalExercise.java
+++ app/src/main/java/cosc345/app/model/IntervalExercise.java
@@ -13,61 +13,41 @@ public class IntervalExercise extends Grader{
     public Interval interval;
 
     public IntervalExercise(Difficulty difficulty){
-        ArrayList<Note> notes = new ArrayList<>();
-        if (difficulty == Difficulty.EASY){
-            notes = pickIntervalEasy();
-        } else if(difficulty == Difficulty.MEDIUM){
-            notes = pickIntervalMedium();
-        } else if(difficulty == Difficulty.HARD){
-            notes = pickIntervalHard();
-        }
-
-        super.notes = notes;
+        super.notes = pickInterval(difficulty);
     }
-    //Perfect Unison Major 3rd
-    public ArrayList<Note> pickIntervalEasy(){
-        // so here you would need to decide the bass note
-        ArrayList<Note> exercise = new ArrayList<>();
-        Intervals[] exerciseConstraints = {Intervals.P1,Intervals.P4, Intervals.P5, Intervals.P8};
-        double invertProbability = 0.1;
-        Boolean invert;
-        Intervals intervalPick = exerciseConstraints[random.nextInt(exerciseConstraints.length)];
-        if (random.nextDouble() < invertProbability){
-            invert = true;
-        } else {
-            invert = false;
-        }
-        this.interval = new Interval(Note.getRandom(), intervalPick, invert); //need to add notes
-        exercise.add(this.interval.root);
-        exercise.add(this.interval.other);
-        return exercise;
 
-    }
-    public ArrayList<Note> pickIntervalMedium(){
-        ArrayList<Note> exercise = new ArrayList<>();
-        Intervals[] exerciseConstraints = {Intervals.P1,Intervals.P4, Intervals.P5, Intervals.P8,
-        Intervals.M2, Intervals.m3, Intervals.m6, Intervals.M6};
-        double invertProbability = 0.25;
-        Boolean invert;
-        Intervals intervalPick = exerciseConstraints[random.nextInt(exerciseConstraints.length)];
-        if (random.nextDouble() < invertProbability){
-            invert = true;
-        } else {
-            invert = false;
-        }
-        this.interval = new Interval(Note.getRandom(), intervalPick, invert); //need to add notes
-        exercise.add(this.interval.root);
-        exercise.add(this.interval.other);
-        return exercise;
+    public ArrayList<Note> pickInterval(Difficulty difficulty){
 
+        Intervals[] exerciseConstraints;
+        double invertProbability;
 
-    }
-    public ArrayList<Note> pickIntervalHard(){
-        ArrayList<Note> exercise = new ArrayList<>();
-        Intervals[] exerciseConstraints = {Intervals.P1,Intervals.P4, Intervals.P5, Intervals.P8,
+        Intervals[] exerciseConstraintsEasy = {Intervals.P1,Intervals.P4, Intervals.P5, Intervals.P8};
+
+        Intervals[] exerciseConstraintsMedium = {Intervals.P1,Intervals.P4, Intervals.P5, Intervals.P8,
+                Intervals.M2, Intervals.m3, Intervals.m6, Intervals.M6};
+        Intervals[] exerciseConstraintsHard = {Intervals.P1,Intervals.P4, Intervals.P5, Intervals.P8,
                 Intervals.M2, Intervals.m3, Intervals.m6, Intervals.M6, Intervals.m2, Intervals.A4,
-        Intervals.m7, Intervals.M7};
-        double invertProbability = 0.5;
+                Intervals.m7, Intervals.M7};
+
+        if (difficulty == Difficulty.EASY){
+
+            exerciseConstraints = exerciseConstraintsEasy;
+            invertProbability = 0.1;
+
+
+        } else if(difficulty == Difficulty.MEDIUM){
+
+            exerciseConstraints = exerciseConstraintsMedium;
+            invertProbability = 0.25;
+
+
+        } else{
+
+            exerciseConstraints = exerciseConstraintsHard;
+            invertProbability = 0.5;
+
+        }
+        ArrayList<Note> exercise = new ArrayList<>();
         Boolean invert;
         Intervals intervalPick = exerciseConstraints[random.nextInt(exerciseConstraints.length)];
         if (random.nextDouble() < invertProbability){
@@ -79,7 +59,6 @@ public class IntervalExercise extends Grader{
         exercise.add(this.interval.root);
         exercise.add(this.interval.other);
         return exercise;
-
 
     }
 }
