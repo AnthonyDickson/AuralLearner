443abb7fefd6a54aa8b9657c9b3d7e532542e928
Anthony Dickson <eight0153@gmail.com>
2018-09-26 08:24:51 +1200

    update Interval unit tests.

--- app/src/test/java/cosc345/app/IntervalUnitTest.java
+++ app/src/test/java/cosc345/app/IntervalUnitTest.java
@@ -3,6 +3,7 @@ package cosc345.app;
 import org.junit.Test;
 
 import cosc345.app.model.Interval;
+import cosc345.app.model.Intervals;
 import cosc345.app.model.Note;
 
 import static org.junit.Assert.assertEquals;
@@ -13,8 +14,8 @@ import static org.junit.Assert.assertEquals;
 public class IntervalUnitTest {
     @Test
     public void createFromInterval() {
-        Interval p1 = new Interval(new Note("C4"), Interval.Intervals.P1);
-        assertEquals(Interval.Intervals.P1, p1.interval);
+        Interval p1 = new Interval(new Note("C4"), Intervals.P1);
+        assertEquals(Intervals.P1, p1.interval);
         assertEquals("Perfect unison (C4, C4)", p1.toString());
         assertEquals("Perfect unison", p1.name.toString());
         assertEquals("Perfect unison", p1.name.getFullName());
@@ -23,8 +24,8 @@ public class IntervalUnitTest {
         assertEquals("C4", p1.other.getName());
         assertEquals(0, p1.size);
 
-        Interval a4 = new Interval(new Note("C4"), Interval.Intervals.A4);
-        assertEquals(Interval.Intervals.A4, a4.interval);
+        Interval a4 = new Interval(new Note("C4"), Intervals.A4);
+        assertEquals(Intervals.A4, a4.interval);
         assertEquals("Augmented fourth", a4.name.toString());
         assertEquals("F#4", a4.other.getName());
         assertEquals(6, a4.size);
@@ -33,15 +34,15 @@ public class IntervalUnitTest {
     @Test
     public void createFromNotes() {
         Interval interval = new Interval(new Note("C4"), new Note("G4"));
-        assertEquals(Interval.Intervals.P5, interval.interval);
+        assertEquals(Intervals.P5, interval.interval);
         assertEquals("Perfect fifth", interval.name.toString());
         assertEquals(7, interval.size);
     }
 
     @Test
     public void createInversions() {
-        Interval inverted = new Interval(new Note("C4"), Interval.Intervals.P5, true);
-        assertEquals(Interval.Intervals.P4, inverted.interval);
+        Interval inverted = new Interval(new Note("C4"), Intervals.P5, true);
+        assertEquals(Intervals.P4, inverted.interval);
         assertEquals("Perfect fourth (G4, C5)", inverted.toString());
         assertEquals(5, inverted.size);
     }
@@ -49,11 +50,11 @@ public class IntervalUnitTest {
     @Test
     public void outOfRangeIntervals() {
         Interval interval = new Interval(new Note("C4"), new Note("G5"));
-        assertEquals(Interval.Intervals.P5, interval.interval);
+        assertEquals(Intervals.P5, interval.interval);
         assertEquals("Perfect fifth", interval.name.toString());
 
         Interval interval2 = new Interval(new Note("G5"), new Note("C4"));
-        assertEquals(Interval.Intervals.P5, interval2.interval);
+        assertEquals(Intervals.P5, interval2.interval);
         assertEquals("Perfect fifth", interval2.name.toString());
     }
 }
