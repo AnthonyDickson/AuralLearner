b21420404bb92658cefd17f272235b16705b46da
Jmannn <manjo773@student.otago.ac.nz>
2018-09-26 23:59:58 +1200

    Partially created interval menu and exercise

--- app/src/main/java/cosc345/app/controller/IntervalsMenu.java
+++ app/src/main/java/cosc345/app/controller/IntervalsMenu.java
@@ -1,5 +1,6 @@
 package cosc345.app.controller;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
@@ -7,18 +8,23 @@ import android.widget.Button;
 
 import java.util.Objects;
 
+import cosc345.app.MainActivity;
 import cosc345.app.R;
 import cosc345.app.model.Difficulty;
+import cosc345.app.model.Intervals;
 
 public class IntervalsMenu extends VoiceControlActivity {
-    private Button easy;
-    private Button medium;
-    private Button hard;
+    private Button easyBtn;
+    private Button mediumBtn;
+    private Button hardBtn;
 
+    //needs voice control stuff
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intervals_menu);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+
     }
 
     @Override
@@ -31,5 +37,21 @@ public class IntervalsMenu extends VoiceControlActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void exerciseEasy(android.view.View view){
+        Intent intent = new Intent(IntervalsMenu.this, IntervalExercise.class);
+        intent.putExtra("EXTRA_DIFFICULTY", "Easy");
+
+    }
+    public void exerciseMedium(android.view.View view){
+        Intent intent = new Intent(IntervalsMenu.this, IntervalExercise.class);
+        intent.putExtra("EXTRA_DIFFICULTY", "Medium");
+
+    }
+    public void exerciseHard(android.view.View view){
+        Intent intent = new Intent(IntervalsMenu.this, IntervalExercise.class);
+        intent.putExtra("EXTRA_DIFFICULTY", "Hard");
+
     }
 }
