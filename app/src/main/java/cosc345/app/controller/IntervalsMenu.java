27bec07b51ae1ef11a233ec34aaaa27c284cc17d
Jmannn <manjo773@student.otago.ac.nz>
2018-09-26 21:45:52 +1200

    Created view for interval menu

--- app/src/main/java/cosc345/app/controller/IntervalsMenu.java
+++ app/src/main/java/cosc345/app/controller/IntervalsMenu.java
@@ -3,12 +3,18 @@ package cosc345.app.controller;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
+import android.widget.Button;
 
 import java.util.Objects;
 
 import cosc345.app.R;
+import cosc345.app.model.Difficulty;
 
 public class IntervalsMenu extends VoiceControlActivity {
+    private Button easy;
+    private Button medium;
+    private Button hard;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intervals_menu);
