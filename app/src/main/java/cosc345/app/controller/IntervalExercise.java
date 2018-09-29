74cc8000ef375999788fc8fea8e65f063ad1d3d1
Anthony Dickson <eight0153@gmail.com>
2018-09-29 13:04:21 +1200

    replace TextToSpeechManager in IntervalExercise.
    It was buggy so doing it manually will avoid any issues for now.

--- app/src/main/java/cosc345/app/controller/IntervalExercise.java
+++ app/src/main/java/cosc345/app/controller/IntervalExercise.java
@@ -2,6 +2,7 @@ package cosc345.app.controller;
 
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
+import android.speech.tts.UtteranceProgressListener;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -16,16 +17,18 @@ import cosc345.app.model.Difficulty;
 import cosc345.app.model.Interval;
 import cosc345.app.model.IntervalExerciseGrader;
 import cosc345.app.model.Playable;
+import cosc345.app.model.State;
 import cosc345.app.model.TextToSpeechManager;
 import cosc345.app.model.VoiceRecognitionManager;
 
 public class IntervalExercise extends AppCompatActivity implements Playable.Delegate {
+    private static final String LOG_TAG = "IntervalExercise";
     private Button startBtn;
     private Button stopBtn;
     Interval targetInterval;
 
     private IntervalExerciseGrader intervalExerciseGrader;
-    public TextToSpeechManager tts;
+    public TextToSpeech tts;
     private Difficulty difficulty;
     private int timesPlayed;
 
@@ -43,8 +46,28 @@ public class IntervalExercise extends AppCompatActivity implements Playable.Dele
         startBtn.setOnClickListener(v -> startExercise()); //startExercise listening, play exercise
         stopBtn.setOnClickListener(v -> stopExercise());
 
-        tts = TextToSpeechManager.getInstance();
-        tts.init(this, null, null);
+        tts = new TextToSpeech(this, status -> {
+            if (status != TextToSpeech.ERROR) {
+                tts.setLanguage(Locale.UK);
+                tts.setOnUtteranceProgressListener(new UtteranceProgressListener() {
+                    @Override
+                    public void onStart(String utteranceId) {
+                        Log.i(LOG_TAG, "Utterance started");
+                    }
+
+                    @Override
+                    public void onDone(String utteranceId) {
+                        Log.i(LOG_TAG, "Utterance finished");
+                    }
+
+                    @Override
+                    public void onError(String utteranceId) {
+                        Log.e(LOG_TAG, String.format("An error occurred when speaking utterance %s.", utteranceId));
+                    }
+                });
+                Log.i(LOG_TAG, "Initialisation Complete.");
+            }
+        });
 
         String difficulty = getIntent().getStringExtra("EXTRA_DIFFICULTY");
 
@@ -62,7 +85,7 @@ public class IntervalExercise extends AppCompatActivity implements Playable.Dele
         super.onPause();
 
         stopExercise();
-        tts.close();
+        tts.shutdown();
     }
 
     private void startExercise() {
@@ -108,13 +131,13 @@ public class IntervalExercise extends AppCompatActivity implements Playable.Dele
         double grade = intervalExerciseGrader.getScore();
 
         if (grade < 60.0) {
-            tts.speak("Your score was bad");
+            tts.speak("Your score was bad", TextToSpeech.QUEUE_FLUSH, null);
         } else if (grade < 80.0) {
-            tts.speak("Your score was ok");
+            tts.speak("Your score was ok", TextToSpeech.QUEUE_FLUSH, null);
         } else if (grade < 90.0) {
-            tts.speak("Your score was good");
+            tts.speak("Your score was good", TextToSpeech.QUEUE_FLUSH, null);
         } else {
-            tts.speak("Your score was perfect");
+            tts.speak("Your score was perfect", TextToSpeech.QUEUE_FLUSH, null);
         }
     }
 
