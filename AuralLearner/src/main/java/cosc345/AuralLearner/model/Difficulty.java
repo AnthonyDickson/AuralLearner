0ab9775385a8996b9ae7779773f8380eb98455b7
travis-ci <travis@travis-ci.org>
2018-10-01 08:48:56 +0000

    Lastest javadoc on successful travis build 63 auto-pushed to gh-pages

--- AuralLearner/src/main/java/cosc345/AuralLearner/model/Difficulty.java
+++
@@ -1,3 +0,0 @@
-package cosc345.AuralLearner.model;
-/* Simple enum for exercise difficulty selection */
-public enum Difficulty {EASY, MEDIUM, HARD}
--- AuralLearner/src/main/java/cosc345/AuralLearner/model/Intervals.java
+++
@@ -1,8 +0,0 @@
-package cosc345.AuralLearner.model;
-
-/**
- * Captures short names of musical intervals.
- */
-public enum Intervals {
-    P1, m2, M2, m3, M3, P4, A4, P5, m6, M6, m7, M7, P8
-}
